942e880ca5dbf8db7c3b64800d008c1fde45ea66
erotufelixpascal2@gmail.com <erotufelixpascal2@gmail.com>
2023-09-08 10:02:53 +0300

    Pass on all tests

--- server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -12,9 +12,15 @@ describe('a fahrenheitToCelsius function should', () => {
 	expect(fahrenheitToCelsius(32)).toBe(0);
   });
   it('return 10 for 50', () => {
-	  expect(fahrenheitToCelsius(50)).toBe(10);
+	expect(fahrenheitToCelsius(50)).toBe(10);
   });
-  it.todo('return 100 for 212');
-  it.todo('return -40 for -40');
-  it.todo('return -273.15 for -459.67');
+  it('return 100 for 212', () => {
+	expect(fahrenheitToCelsius(212)).toBe(100);  
+  });
+  it('return -40 for -40', () => {
+	expect(fahrenheitToCelsius(-40)).toBe(-40);  
+  });
+  it('return -273.15 for -459.67', () => {
+	expect(fahrenheitToCelsius(-459.67)).toBe(-273.15); 
+  });
 });
